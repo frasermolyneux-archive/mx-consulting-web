304d527da5bc7911b1a5e046e6d28f6d9b65ac35
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-22 15:03:39 +0000

    Adding terraform to create resource group and app service plan

---
+++ terraform/web-app.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "resource-group" {
+    name = "MX-Web-${var.environment}"
+    location = "${var.region}"
+}
+
+resource "azurerm_app_service_plan" "app-service-plan" {
+    name = "MX-Web-AppPlan-${var.environment}"
+    resource_group_name = "${azurerm_resource_group.resource-group.name}"
+    location = "${azurerm_resource_group.resource-group.location}"
+    sku {
+        tier = "Basic"
+        size = "B1"
+    }
+}
