127c67eaebb29980643c69fbb63bf39e6d4588e0
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-22 16:44:32 +0000

    Adding resource to create web app

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -13,3 +13,10 @@ resource "azurerm_app_service_plan" "app-service-plan" {
         size = "B1"
     }
 }
+
+resource "azurerm_app_service" "app-service" {
+  name = "MX-WebApp-${var.environment}"
+  location = "${var.region}"
+  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.app-service-plan.id}"
+}
