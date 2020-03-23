557ab8e405a3ec14b3d94e634317a2168d5aa5f8
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-23 17:59:50 +0000

    Enabling domain bindings, all should now be valid

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -21,26 +21,23 @@ resource "azurerm_app_service" "app-service" {
   app_service_plan_id = "${azurerm_app_service_plan.app-service-plan.id}"
 }
 
-//resource "azurerm_app_service_custom_hostname_binding" "molyneux-consulting-co-uk" {
-//  hostname = "molyneux-consulting.co.uk"
-//  app_service_name = "${azurerm_app_service.app-service.name}"
-//  resource_group_name = "${azurerm_resource_group.resource-group.name}"
-//}
-
-//resource "azurerm_app_service_custom_hostname_binding" "www-molyneux-consulting-co-uk" {
-//  hostname = "www.molyneux-consulting.co.uk"
-//  app_service_name = "${azurerm_app_service.app-service.name}"
-//  resource_group_name = "${azurerm_resource_group.resource-group.name}"
-//}
-
-//resource "azurerm_app_service_custom_hostname_binding" "mx-consulting-co-uk" {
-//  hostname = "mx-consulting.co.uk"
-//  app_service_name = "${azurerm_app_service.app-service.name}"
-//  resource_group_name = "${azurerm_resource_group.resource-group.name}"
-//}
-
-//resource "azurerm_app_service_custom_hostname_binding" "www-mx-consulting-co-uk" {
-//  hostname = "www.mx-consulting.co.uk"
-//  app_service_name = "${azurerm_app_service.app-service.name}"
-//  resource_group_name = "${azurerm_resource_group.resource-group.name}"
-//}
+resource "azurerm_app_service_custom_hostname_binding" "molyneux-consulting-co-uk" {
+  hostname = "molyneux-consulting.co.uk"
+  app_service_name = "${azurerm_app_service.app-service.name}"
+  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+}
+resource "azurerm_app_service_custom_hostname_binding" "www-molyneux-consulting-co-uk" {
+  hostname = "www.molyneux-consulting.co.uk"
+  app_service_name = "${azurerm_app_service.app-service.name}"
+  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+}
+resource "azurerm_app_service_custom_hostname_binding" "mx-consulting-co-uk" {
+  hostname = "mx-consulting.co.uk"
+  app_service_name = "${azurerm_app_service.app-service.name}"
+  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+}
+resource "azurerm_app_service_custom_hostname_binding" "www-mx-consulting-co-uk" {
+  hostname = "www.mx-consulting.co.uk"
+  app_service_name = "${azurerm_app_service.app-service.name}"
+  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+}
