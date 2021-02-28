6ecdc5a6899faf4fd8270330a074b78da87a309a
Fraser Molyneux <websites@mx-mail.io>
2021-02-28 13:47:15 +0000

    Updating to .NET 5

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -13,4 +13,8 @@ resource "azurerm_app_service" "app-service" {
   location = var.region
   resource_group_name = azurerm_resource_group.resource-group.name
   app_service_plan_id = azurerm_app_service_plan.app-service-plan.id
+
+  site_config {
+    dotnet_framework_version  = "v5.0"
+  }
 }
